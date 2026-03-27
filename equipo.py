156176674f700725c6db9e01a6885f06ef224be7
CristianMonsalveDuque <ccmduque@gmail.com>
2026-03-27 11:08:09 -0500

    Saludo Cristian

--- equipo.py
+++ equipo.py
@@ -1,10 +1,13 @@
 print("Práctica de trabajo colaborativo")
 
-integrantes = []
+integrantes = ["Cristian"]
 
 def mostrar_integrantes():
     print("\nIntegrantes del equipo:")
     for persona in integrantes:
         print("- " + persona)
+
+        def saludo_cristian():
+            print("\nEstoy en el grupo.")
 
 print("Aquí iremos agregando aportes.")
