d68e6860f9191075a4e96ce3e42cc1ab0c0329bd
JavierETA <javier.elias.tobon.ayubb30@gmail.com>
2026-03-27 10:58:32 -0500

    Se agrega equipo.py

---
+++ equipo.py
@@ -0,0 +1,10 @@
+print("Práctica de trabajo colaborativo")
+
+integrantes = []
+
+def mostrar_integrantes():
+    print("\nIntegrantes del equipo:")
+    for persona in integrantes:
+        print("- " + persona)
+
+print("Aquí iremos agregando aportes.")
